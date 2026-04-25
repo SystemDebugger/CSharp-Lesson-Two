3f342b53e1fba3fc6de61bbf2728d91a4c731c65
SystemDebugger <system.debugger.sd@gmail.com>
2026-04-25 11:02:56 -0400

    Update allowed release version

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// See https://aka.ms/new-console-template for more information.
 Console.WriteLine("Hello, World!");
+string myName = "Allendro";
+Console.WriteLine("Hello, " + myName);
+int myAge = 16;
+double myHeight = 5 + (5 / 12);
+Console.WriteLine($"I am {myAge} years old and {myHeight} feet tall.");
