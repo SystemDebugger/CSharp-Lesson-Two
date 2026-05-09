aa78cb38d0a172fbf7be244c1c33e857151a7999
SystemDebugger <system.debugger.sd@gmail.com>
2026-05-09 11:57:06 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information.
-Console.WriteLine("Hello, World!");
-string myName = "Allendro";
-Console.WriteLine("Hello, " + myName);
-int myAge = 16;
-double myHeight = 5 + (5 / 12);
-Console.WriteLine($"I am {myAge} years old and {myHeight} feet tall.");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        string myName = "Allendro";
+        Console.WriteLine("Hello, " + myName);
+        int myAge = 16;
+        double myHeight = 5 + (5.0 / 12.0);
+        Console.WriteLine($"I am {myAge} years old and {myHeight} feet tall.");
+        Console.WriteLine("\nWhat is your name?");
+        Info();
+    }
+
+    static void Info()
+    {
+        string name = Console.ReadLine();
+
+        if (name.ToLower() == "allendro")
+        {
+            Console.WriteLine("\nThat is my name, please try again.");
+            Info();
+        }
+        else if (name != null)
+        {
+            Console.WriteLine("\nHello " + name + ", nice to meet you!");
+        }
+        else
+        {
+            Console.WriteLine("\nYou haven't entered a name. Are you sure?");
+            string res = Console.ReadLine();
+            if (res.ToLower() == "yes")
+            {
+                Console.WriteLine("\nHello, nice to meet you!");
+            }
+            else
+            {
+                Console.WriteLine("\nAlright, let's get you a name.");
+                Info();
+            }
+        }
+    }
+}
